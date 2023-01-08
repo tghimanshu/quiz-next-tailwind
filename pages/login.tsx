9321b0ab9da242255820a528246b4cff84d1d8f7
Himanshu Gohil <himanshugohil234@gmail.com>
2023-01-08 11:50:50 +0530

    added home page

---
+++ pages/login.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="bg-black m-0">
+      <div className="bg-white w-full h-[80vh]"></div>
+    </div>
+  );
+};
+
+export default Login;
